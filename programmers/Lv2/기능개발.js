a5121ae6e6a776417769a84bb7fc111ffe046f03
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-06-04 09:15:58 +0900

    기능개발

---
+++ programmers/Lv2/기능개발.js
@@ -0,0 +1,35 @@
+function solution(progresses, speeds) {
+  var answer = [];
+  let durationRequired = [];
+
+  progresses.map((item, idx) => {
+    endCount = 0;
+
+    while (item < 100) {
+      item += speeds[idx];
+      endCount++;
+    }
+
+    durationRequired.push(endCount);
+  });
+
+  let next = 1;
+  for (let i = 0; i < durationRequired.length; i += next) {
+    let count = 1;
+
+    for (let j = i + 1; j <= durationRequired.length; j++) {
+      if (durationRequired[i] >= durationRequired[j]) {
+        count++;
+      } else {
+        answer.push(count);
+        next = count;
+        count = 1;
+        break;
+      }
+    }
+  }
+
+  return answer;
+}
+
+console.log(solution([95, 95, 95, 95], [4, 3, 2, 1]));
