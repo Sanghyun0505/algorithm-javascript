03ce443be82b8b338f7a933b16b72cec5a45ac1c
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-11-28 17:37:32 +0900

    할인행사

---
+++ programmers/Lv2/할인행사.js
@@ -0,0 +1,60 @@
+function solution(want, number, discount) {
+  var answer = 0;
+
+  if (want.length === 1 && !discount.includes(want[0])) {
+    return 0;
+  }
+
+  const numberByProduct = {};
+  want.forEach((item, idx) => (numberByProduct[item] = number[idx]));
+
+  let sumOfProduct = 0;
+  number.forEach((num) => (sumOfProduct += num));
+
+  while (discount.length >= sumOfProduct) {
+    let count = 0;
+    const a = { ...numberByProduct };
+
+    for (let i = 0; i < 10; i++) {
+      const discountValue = discount[i];
+
+      if (!isNaN(a[discountValue]) && a[discountValue] > 0) {
+        a[discountValue] -= 1;
+        count++;
+      }
+    }
+
+    if (count === sumOfProduct) {
+      answer++;
+    }
+
+    discount.shift();
+  }
+
+  return answer;
+}
+
+console.log(
+  solution(
+    ["banana", "apple", "rice", "pork", "pot"],
+    [3, 2, 2, 2, 1],
+    [
+      "chicken",
+      "apple",
+      "apple",
+      "banana",
+      "rice",
+      "apple",
+      "pork",
+      "banana",
+      "pork",
+      "rice",
+      "pot",
+      "banana",
+      "apple",
+      "banana",
+    ]
+  )
+);
+
+// 3
